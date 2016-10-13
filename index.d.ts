0cec891f4040c7745990f74c16da0b42e233d58c
Krill Huang <Krill Huang>
2016-10-13 22:37:53 +0800

    update

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export declare function memorizer(func: Function, resolver: Function, cache?: Map<any, any>, ctx?: any): any;
+export declare function shellMemorizer(fundamental: Function, resolver: Function, cache: Map<any, any>, ctx: any): any;
